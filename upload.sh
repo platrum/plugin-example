abad27a9d551b7a0acc26357aeae9bce7b590a9b
Бендриков Федор <bendrikov.f@roistat.com>
2021-08-03 01:36:30 +0300

    dev

--- upload.sh
+++ upload.sh
@@ -41,5 +41,5 @@ zip -r plugin.zip .
 
 curl "${PLATRUM_HOST}/plugins/api/plugin/upload?${PLATRUM_AUTH}" \
   -H 'Content-Type: application/json' \
-  -X 'GET' >> /dev/null \
+  -X 'GET' \
   --data-binary "@./plugin.zip"
